56dd8c5f8737e734b78a23551a088eb545b72d92
luxing <1151368955@qq.com>
2019-07-11 17:45:03 +0800

    1

---
+++ test/TestClass/ComposerTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luxing
+ * Date: 2019/7/11
+ * Time: 17:36
+ */
+
+namespace Test\TestClass;
+
+class ComposerTest
+{
+
+    public static function test(){
+        echo "Hello world !";
+    }
+
+}
