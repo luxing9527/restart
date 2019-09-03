bd2cc3667b28bd1fa092d91b0a383b4430f6bd29
luxing <1151368955@qq.com>
2019-09-03 15:36:02 +0800

    2

---
+++ test/TestClass/Haha.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luxing
+ * Date: 2019/9/3
+ * Time: 15:30
+ */
+
+namespace Test\TestClass;
+
+
+class Haha
+{
+
+    public static function haha(){
+        echo "哈哈哈哈哈哈";
+    }
+
+}
